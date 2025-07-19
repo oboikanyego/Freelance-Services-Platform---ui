d03827ba384a2150ae628aa06925f5476cc2e924
oboikanyego <kanyiradipabe@gmail.com>
2025-07-19 10:33:54 +0200

    - smart loader delay: If api is too fast,show loader for 3 more seconds

--- src/app/services/loading.ts
+++ src/app/services/loading.ts
@@ -4,18 +4,27 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class Loading {
+export class Loading  {
   private loadingSubject = new BehaviorSubject<boolean>(false);
   loading$ = this.loadingSubject.asObservable();
 
+  private startTime: number | null = null;
+  private readonly minVisibleTime = 1000; // Minimum 1s loader display
+
   setLoading(isLoading: boolean) {
     if (isLoading) {
+      this.startTime = Date.now();
       this.loadingSubject.next(true);
     } else {
-      // Delay hiding by 3 seconds
-      setTimeout(() => {
+      const elapsed = Date.now() - (this.startTime ?? 0);
+      const remaining = this.minVisibleTime - elapsed;
+
+      if (remaining > 0) {
+        setTimeout(() => this.loadingSubject.next(false), remaining);
+      } else {
         this.loadingSubject.next(false);
-      }, 3000);
+      }
+      this.startTime = null;
     }
   }
 }
